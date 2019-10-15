45b3cfb25fb630a286b062e62d2c176878427bda
saneki <s@neki.me>
2019-10-15 08:59:59 -0500

    Add n64romtool binary (does nothing yet)

---
+++ src/bin/n64romtool.rs
@@ -0,0 +1,22 @@
+use clap::{App, Arg};
+use std::error::Error;
+use std::fs::File;
+use std::path::Path;
+use n64rom::header::N64Header;
+
+fn main() -> Result<(), Box<Error>> {
+    let matches = App::new("n64romtool")
+        .author("saneki <s@neki.me>")
+        .about("Displays information about N64 ROM files")
+        .arg(Arg::with_name("FILE")
+            .required(true))
+        .get_matches();
+
+    let in_path = Path::new(matches.value_of("FILE").unwrap());
+    let header = {
+        let mut file = File::open(in_path)?;
+        N64Header::read(&mut file)
+    };
+
+    Ok(())
+}

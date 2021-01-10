78b777a4cb2b421952dd48b573fb7e4c198c8d5d
saneki <s@neki.me>
2021-01-09 18:58:48 -0600

    header.rs: Organize functions by name

--- src/header.rs
+++ src/header.rs
@@ -41,14 +41,14 @@ impl fmt::Display for Magic {
 impl Magic {
     pub const SIZE: usize = 4;
 
+    /// Infer the byte order (endianness) of the following data.
+    pub fn byte_order(&self) -> Result<Endianness, Error> {
+        Magic::infer_byte_order(&self.0)
+    }
+
     // Register: `PI_BSD_DOM1_LAT_REG`.
     pub fn device_latency(&self) -> u8 {
         self.0[0]
-    }
-
-    // Register: `PI_BSD_DOM1_PWD_REG`.
-    pub fn device_rw_pulse_width(&self) -> u8 {
-        self.0[1]
     }
 
     // Register: `PI_BSD_DOM1_PGS_REG`.
@@ -56,9 +56,22 @@ impl Magic {
         self.0[2]
     }
 
+    // Register: `PI_BSD_DOM1_PWD_REG`.
+    pub fn device_rw_pulse_width(&self) -> u8 {
+        self.0[1]
+    }
+
     // Register: `PI_BSD_DOM1_RLS_REG`.
     pub fn device_rw_release_duration(&self) -> u8 {
         self.0[3]
+    }
+
+    /// Construct using at least 4 bytes.
+    pub fn from(bytes: &[u8]) -> Self {
+        // Cleaner way to do this?
+        let mut magic = Magic::new();
+        magic.0.copy_from_slice(&bytes[..4]);
+        magic
     }
 
     pub fn infer_byte_order(data: &[u8]) -> Result<Endianness, Error> {
@@ -69,19 +82,6 @@ impl Magic {
             0x3780_4012 => Ok(Endianness::Mixed),
             _ => Err(Error::UnknownByteOrder(value)),
         }
-    }
-
-    /// Infer the byte order (endianness) of the following data.
-    pub fn byte_order(&self) -> Result<Endianness, Error> {
-        Magic::infer_byte_order(&self.0)
-    }
-
-    /// Construct using at least 4 bytes.
-    pub fn from(bytes: &[u8]) -> Self {
-        // Cleaner way to do this?
-        let mut magic = Magic::new();
-        magic.0.copy_from_slice(&bytes[..4]);
-        magic
     }
 
     pub fn new() -> Self {
@@ -111,6 +111,11 @@ impl AsRef<[u8; 4]> for Magic {
 pub struct Media([u8; 4]);
 
 impl Media {
+    /// Get slice as string.
+    pub fn as_str(&self) -> Result<&str, Utf8Error> {
+        str::from_utf8(&self.0)
+    }
+
     /// Get all values as `char` tuple.
     pub fn chars(&self) -> (char, char, char, char) {
         (self.0[0] as char, self.0[1] as char, self.0[2] as char, self.0[3] as char)
@@ -119,11 +124,6 @@ impl Media {
     /// Get all values as `u8` tuple.
     pub fn values(&self) -> (u8, u8, u8, u8) {
         (self.0[0], self.0[1], self.0[2], self.0[3])
-    }
-
-    /// Get slice as string.
-    pub fn as_str(&self) -> Result<&str, Utf8Error> {
-        str::from_utf8(&self.0)
     }
 }
 
@@ -204,6 +204,23 @@ impl Header {
         str::from_utf8(&self.name)
     }
 
+    /// Create a new `Header`.
+    pub fn new(entry_point: u32, name: &str, media: &[u8], program: &[u8], fs: &[u8], ipl3: &IPL3) -> Self {
+        let mut header = Self::default();
+        let (crc1, crc2) = ipl3.compute_crcs(program, fs);
+        let name_bytes = &name.as_bytes()[..20];
+        header.magic = Magic::new();
+        header.clock_rate = 15;
+        header.entry_point = ipl3.offset(entry_point);
+        header.release = 0;
+        header.crc1 = crc1;
+        header.crc2 = crc2;
+        header.name.copy_from_slice(name_bytes);
+        header.media.as_mut().copy_from_slice(&media[..4]);
+        header
+    }
+
+    /// Read ordered by converting to big endian.
     pub fn read_ordered<T: Read>(reader: &'_ mut T) -> Result<(Self, Endianness), Error> {
         let mut buf = [0; Header::SIZE];
         reader.read_exact(&mut buf)?;
@@ -232,21 +249,6 @@ impl Header {
         reader.read_exact(header.media.as_mut())?;
         header._reserved_3 = reader.read_u8()?;
         Ok(header)
-    }
-
-    pub fn new(entry_point: u32, name: &str, media: &[u8], program: &[u8], fs: &[u8], ipl3: &IPL3) -> Self {
-        let mut header = Self::default();
-        let (crc1, crc2) = ipl3.compute_crcs(program, fs);
-        let name_bytes = &name.as_bytes()[..20];
-        header.magic = Magic::new();
-        header.clock_rate = 15;
-        header.entry_point = ipl3.offset(entry_point);
-        header.release = 0;
-        header.crc1 = crc1;
-        header.crc2 = crc2;
-        header.name.copy_from_slice(name_bytes);
-        header.media.as_mut().copy_from_slice(&media[..4]);
-        header
     }
 
     pub fn write<T: Write>(&self, writer: &'_ mut T) -> io::Result<usize> {
